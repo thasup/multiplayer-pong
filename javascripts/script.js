ae659e6f98d21894026faa7b60824312f20f9a03
thasup <thanachonfirst@hotmail.com>
2022-11-01 23:47:57 +0700

    Edit old pong script

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,7 +2,7 @@
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 let paddleIndex = 0;
-
+const socket = io('http://localhost:3000');
 let width = 500;
 let height = 700;
 
@@ -10,8 +10,8 @@ let height = 700;
 let paddleHeight = 10;
 let paddleWidth = 50;
 let paddleDiff = 25;
-let paddleX = [ 225, 225 ];
-let trajectoryX = [ 0, 0 ];
+let paddleX = [225, 225];
+let trajectoryX = [0, 0];
 let playerMoved = false;
 
 // Ball
@@ -23,10 +23,9 @@ let ballDirection = 1;
 // Speed
 let speedY = 2;
 let speedX = 0;
-let computerSpeed = 4;
 
 // Score for Both Players
-let score = [ 0, 0 ];
+let score = [0, 0];
 
 // Create Canvas Element
 function createCanvas() {
@@ -38,16 +37,16 @@ function createCanvas() {
 }
 
 // Wait for Opponents
-// function renderIntro() {
-//   // Canvas Background
-//   context.fillStyle = 'black';
-//   context.fillRect(0, 0, width, height);
+function renderIntro() {
+  // Canvas Background
+  context.fillStyle = 'black';
+  context.fillRect(0, 0, width, height);
 
-//   // Intro Text
-//   context.fillStyle = 'white';
-//   context.font = "32px Courier New";
-//   context.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30);
-// }
+  // Intro Text
+  context.fillStyle = 'white';
+  context.font = "32px Courier New";
+  context.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30);
+}
 
 // Render Everything on Canvas
 function renderCanvas() {
@@ -146,35 +145,14 @@ function ballBoundaries() {
       trajectoryX[1] = ballX - (paddleX[1] + paddleDiff);
       speedX = trajectoryX[1] * 0.3;
     } else {
-      // Reset Ball, Increase Computer Difficulty, add to Player Score
-      if (computerSpeed < 6) {
-        computerSpeed += 0.5;
-      }
       ballReset();
       score[0]++;
     }
   }
 }
 
-// Computer Movement
-function computerAI() {
-  if (playerMoved) {
-    if (paddleX[1] + paddleDiff < ballX) {
-      paddleX[1] += computerSpeed;
-    } else {
-      paddleX[1] -= computerSpeed;
-    }
-    if (paddleX[1] < 0) {
-      paddleX[1] = 0;
-    } else if (paddleX[1] > (width - paddleWidth)) {
-      paddleX[1] = width - paddleWidth;
-    }
-  }
-}
-
 // Called Every Frame
 function animate() {
-  computerAI();
   ballMove();
   renderCanvas();
   ballBoundaries();
@@ -184,8 +162,8 @@ function animate() {
 // Start Game, Reset Everything
 function startGame() {
   createCanvas();
-  // renderIntro();
-  
+  renderIntro();
+
   paddleIndex = 0;
   window.requestAnimationFrame(animate);
   canvas.addEventListener('mousemove', (e) => {
